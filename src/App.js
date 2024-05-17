b1cd700cfc01d8b2bc635a43971e52d053bded3b
Rogelio Menco <rogeliomenco4@gmail.com>
2024-05-16 19:35:11 -0500

    create initial and empty project structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+
+      <h1>Hello world i'm Rogelio Menco Benavides!!</h1>
+    
+    </div>
+  );
+}
+
+export default App;
